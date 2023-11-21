0bae2f485084a11ba0e5c7fdba7b8920ba6bcaf4
knightalli <knightalli@mail.ru>
2023-11-21 22:39:33 +0500

    home.1

--- online-store-for-pizzeria.client/src/Home.jsx
+++ online-store-for-pizzeria.client/src/Home.jsx
@@ -1,59 +1,71 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import present from './image/present.png';
+import data from "./Data.json";
 import pizFoCh from './image/pizFourCheese.jpg';
-import Header from './components/Header';
-import Footer from './components/Footer';
 
 function Home() {
-    return ( 
+    const [searchTerm, setSearchTerm] = useState("");
+
+    let images = [pizFoCh];
+
+        return ( 
         <div className="home-div">
+            <div className="container">
+                <div className="search-container">
+                    <input id="search" type="text" placeholder="Search here..." onChange={(event) => {
+                            setSearchTerm(event.target.value);
+                        }} />
+                </div>
+            </div>
             <div className='body-home'>
                 <div className='slidebar'>
                     <ul className='categories'>
-                    <li className='categories-item'>Недавние заказы</li>
-                    <li className='categories-item'>Популярные</li>
-                    <li className='categories-item'>Мясные</li>
-                    <li className='categories-item'>Вегетарианские</li>
-                    <li className='categories-item'>Малокалорийные</li>
-                    <li className='categories-item'>Сытные</li>
+                        <li className='categories-item'>Недавние заказы</li>
+                        <li className='categories-item'>Популярные</li>
+                        <li className='categories-item'>Мясные</li>
+                        <li className='categories-item'>Вегетарианские</li>
+                        <li className='categories-item'>Малокалорийные</li>
+                        <li className='categories-item'>Сытные</li>
                     </ul>
                 </div>
                 <div className='discount'>
                     <ul className='discount-list'>
-                    <li className='discount-list-item'>
-                        <h2 className='present-h'>2=1!</h2>
-                        <img className='present-img' src={present} alt='present' width={64} height={64}/>
-                        <p className='present-discription'>Две пиццы по цене одной!</p> 
-                        <p className='present-promo'>Промокод:</p>
-                        <p className='present-code'>DUCK21</p>
-                    </li>
+                        <li className='discount-list-item'>
+                            <h2 className='present-h'>2=1!</h2>
+                            <img className='present-img' src={present} alt='present' width={64} height={64}/>
+                            <p className='present-description'>Две пиццы по цене одной!</p> 
+                            <p className='present-promo'>Промокод:</p>
+                            <p className='present-code'>DUCK21</p>
+                        </li>
                     </ul>
                 </div>
-                <div className='pizzas'>
-                    <ul className='pizzas-list'>
-                    <li className='pizzas-list-item'>
-                        <img src={pizFoCh} alt='Пицца четыре сыра' width={278} height={267}/>
-                        <p className='hit'>Хит!</p>
-                        <p className='pizza-price'>700р.</p>
-                        <h2 className='pizza-name'>Пицца 4 сыра</h2>
-                        <p className='pizza-description'>Вкусная, ароматная пицца с пикантными нотками благородных сыров. Попробуйте, и вы не останетесь равнодушными!</p>
-                    </li>
-                    <li className='pizzas-list-item'>
-                        <img src={pizFoCh} alt='Пицца четыре сыра' width={278} height={267}/>
-                        <p className='hit'>Хит!</p>
-                        <p className='pizza-price'>700р.</p>
-                        <h2 className='pizza-name'>Пицца 4 сыра</h2>
-                        <p className='pizza-description'>Вкусная, ароматная пицца с пикантными нотками благородных сыров. Попробуйте, и вы не останетесь равнодушными!</p>
-                    </li>
-                    <li className='pizzas-list-item'>
-                        <img src={pizFoCh} alt='Пицца четыре сыра' width={278} height={267}/>
-                        <p className='hit'>Хит!</p>
-                        <p className='pizza-price'>700р.</p>
-                        <h2 className='pizza-name'>Пицца 4 сыра</h2>
-                        <p className='pizza-description'>Вкусная, ароматная пицца с пикантными нотками благородных сыров. Попробуйте, и вы не останетесь равнодушными!</p>
-                    </li>
-                    </ul>
+                <div className="template_Container">
+                    {
+                        data 
+                            .filter((val) => {
+                                if(searchTerm == ""){
+                                return val;
+                                }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                                return val;
+                                }
+                            })
+                            .map((val) => {
+                                return(
+                                <div className="template" key={val.id}>
+                                    <div className='pizza-image'>                                    
+                                        <img src={images[val.image]} alt={val.title} width={278} height={267}/>
+                                        <div className='hit'>Хит!</div>
+                                        <div className='pizza-price'>{val.price}</div>
+                                    </div>
+                                    <div className='description'>                                        
+                                        <button className='pizza-name'>{val.title}</button>
+                                        <p className='pizza-description'>{val.description}</p>
+                                    </div>
+                                </div> 
+                                )
+                            })
+                    }
                 </div>
             </div>
         </div>
--- online-store-for-pizzeria.client/src/components/Header.jsx
+++ online-store-for-pizzeria.client/src/components/Header.jsx
@@ -1,18 +1,24 @@
-import '../App.css';
+import '../styles/Header.css';
 import logo from '../image/logo.svg';
 import {Link} from 'react-router-dom';
+import React, {useState} from "react";
 
 function Header() {
+    const [searchTerm, setSearchTerm] = useState("");
+
     return (
-        <header className="App-header">            
-            <ul>
-                <li><Link to="/"> <img className='logo-main' src={logo} alt="logo" width={329} height={100}/></Link></li>
-                <li><Link to="/cart">Корзина</Link></li>
-                <li><Link to="/profile">Личный кабинет</Link></li>
-            </ul>
-            <input className='search'></input>
+        <header className="header"> 
+            <div className='logo'><Link to="/"> <img className='logo-main' src={logo} alt="logo" width={329} height={100}/></Link></div>
+            {/* <div className='search-container'>
+                <input className='search' type='text' placeholder="Найти пиццу" onChange={(event) => {
+                            setSearchTerm(event.target.value);
+                        }} />
+            </div>             */}
+            <div className='cart-header'><Link className='cart-header-a' to="/cart">Корзина</Link></div>
+            <div className='profile-header'><Link className='profile-header-a' to="/profile">Личный кабинет</Link></div>            
         </header>
     );
 }
 
 export default Header;
+
