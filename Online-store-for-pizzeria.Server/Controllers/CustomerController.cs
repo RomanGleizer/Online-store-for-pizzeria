9393df2b153002793f8d0a2fcb4ec02f62f19753
Roman <125725530+RomanGleizer@users.noreply.github.com>
2023-11-23 15:19:05 +0500

    Final commit to create authentication and authorization

--- Online-store-for-pizzeria.Server/Controllers/CustomerController.cs
+++ Online-store-for-pizzeria.Server/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Online_store_for_pizzeria.Server.AppContext;
 
@@ -13,7 +14,14 @@ public class CustomerController : Controller
         _applicationContext = context;
     }
 
-    public async Task<IActionResult> Index()
+    [Authorize]
+    public IActionResult Index()
+    {
+        return Content("Hello");
+    }
+
+    [Authorize]
+    public async Task<IActionResult> Customers()
     {
         return View(await _applicationContext.Customers.ToListAsync());
     }
