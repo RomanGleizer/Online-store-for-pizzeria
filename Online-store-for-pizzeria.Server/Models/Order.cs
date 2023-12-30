978e00f1fb31fd28a770f5b0ece0f7204dac4878
Roman <125725530+RomanGleizer@users.noreply.github.com>
2023-12-30 19:33:40 +0600

    time

--- Online-store-for-pizzeria.Server/Models/Order.cs
+++ Online-store-for-pizzeria.Server/Models/Order.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using HtmlAgilityPack;
+using System.Text;
 
 public class Order
 {
@@ -18,10 +19,39 @@ public class Order
 
     public override string ToString()
     {
-        var stringBuilder = new StringBuilder();
-        foreach (var pizza in Pizzas)
-            stringBuilder.Append($"Пицца : {pizza.Title}, количество: {pizza.CartQuantity}\n");
+        var fullPizzaOrderInfo = new PizzaOrder[]
+        {
+            new PizzaOrder { Name = "Карбонара", CreatingTime = 10 },
+            new PizzaOrder { Name = "Сырная", CreatingTime = 8 },
+            new PizzaOrder { Name = "Четыре сезона", CreatingTime = 11 },
+            new PizzaOrder { Name = "Пепперони Фреш", CreatingTime = 8 },
+            new PizzaOrder { Name = "Ветчина и сыр", CreatingTime = 10 }
+        };
 
-        return $"Был сделан заказ на сумму {TotalPrice}, тип оплаты: {PaymentType}, тип доставки: {DeliveryType}, адрес: {Address}\n{stringBuilder.ToString()}";
+        var stringBuilder = new StringBuilder();
+        var time = 0;
+        foreach (var pizza in Pizzas)
+        {
+            foreach (var pizzaInfo in fullPizzaOrderInfo)
+                if (string.Equals(pizza.Title, pizzaInfo.Name, StringComparison.OrdinalIgnoreCase))
+                    time += pizzaInfo.CreatingTime;
+
+            stringBuilder.Append($"Пицца : {pizza.Title}, количество: {pizza.CartQuantity}\n");
+        }
+        
+        return $"Был сделан заказ на сумму {TotalPrice}, тип оплаты: {PaymentType}, тип доставки: {DeliveryType}, адрес: {Address}\n{stringBuilder.ToString()}\nОбщее время изготовления и выпекания всех пицц: {time} мин. Если вы заказали доставку, то курьер вам позвонит вам перед выездом.";
+    }
+
+    private HtmlNodeCollection? GetTrafficData()
+    {
+        var url = "https://xn--80acgfbsl1azdqr.xn----8sbb2ahcjpdbegn.xn--p1ai/";
+        var web = new HtmlWeb();
+        var document = web.Load(url);
+        var node = document.DocumentNode.SelectNodes("//ymaps[@class='ymaps-b-traffic-panel__level']");
+
+        if (node != null)
+            return node;
+        else
+            throw new NotImplementedException("Элемент не найден");
     }
 }
